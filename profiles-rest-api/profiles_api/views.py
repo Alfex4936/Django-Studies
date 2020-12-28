aeab5023ebfc486b409f9d88390d84ca8cf74280
Seok Won <ikr@kakao.com>
2020-12-28 21:34:01 +0900

    Django: Add simple put/patch/delete to APIView
    0.0.0.0:8000/api/hello-view/

--- profiles-rest-api/profiles_api/views.py
+++ profiles-rest-api/profiles_api/views.py
@@ -32,3 +32,15 @@ class HelloApiView(APIView):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def put(self, request, pk=None):  # primary key
+        """Handle updating an object"""
+        return Response({"message": "PUT"})
+
+    def patch(self, request, pk=None):
+        """Handle a partial update of object"""
+        return Response({"message": "PATCH"})
+
+    def delete(self, request, pk=None):
+        """Delete an object"""
+        return Response({"message": "DELETE"})
+
