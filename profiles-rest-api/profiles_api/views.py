a6adb6db25bd39d88fdaaa02463fb000657f1c73
Seok Won <ikr@kakao.com>
2021-01-17 22:13:32 +0900

    Create login API

--- profiles-rest-api/profiles_api/views.py
+++ profiles-rest-api/profiles_api/views.py
@@ -4,6 +4,8 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework import filters
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from profiles_api import serializers
 from profiles_api import models
@@ -101,3 +103,10 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     filter_backends = (filters.SearchFilter,)
     search_fields = ("name", "email")  # what will filter do?
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASS
+
